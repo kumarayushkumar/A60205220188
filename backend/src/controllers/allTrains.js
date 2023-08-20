db83ad7b59b3d4d0e8ffea5142381514e6a77238
Ayush kumar <kumarayushkumar@protonmail.com>
2023-08-20 13:33:03 +0530

    30mintue check added

--- backend/src/controllers/allTrains.js
+++ backend/src/controllers/allTrains.js
@@ -6,12 +6,25 @@ const allTrain = async (req, res) => {
       headers: req.headers,
     });
 
-    const trainDataArray = response.data;
+    const currentTime = new Date(); // Get the current time
+
+    const trainDataArray = response.data.filter(train => {
+      const departureTime = new Date();
+      departureTime.setHours(train.departureTime.Hours);
+      departureTime.setMinutes(train.departureTime.Minutes);
+      departureTime.setSeconds(train.departureTime.Seconds);
+
+      const timeDifferenceInMinutes = (departureTime - currentTime) / (1000 * 60);
+
+      return timeDifferenceInMinutes >= 30; // Include trains with departure time >= 30 minutes
+    });
+
+    console.log(trainDataArray);
 
     res.status(200).json(trainDataArray);
   } catch (error) {
     console.error(error);
-    res.status(500).json({ error: 'An derror occurred' });
+    res.status(500).json({ error: 'An error occurred' });
   }
 }
 
--- backend/src/server.js
+++ backend/src/server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: 'http://localhost:5173',
   credentials: true,
   allowedHeaders: ['Content-Type', 'Authorization', 'Origin', 'X-Requested-With', 'Accept']
 }))
